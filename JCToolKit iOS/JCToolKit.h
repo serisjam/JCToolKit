9f1d720abf34e4cd5a63dba1b6503459ab281527
Jam <hxjiamiao@126.com>
2021-03-29 17:12:09 +0800

    add xcodeproj settings

---
+++ JCToolKit iOS/JCToolKit.h
@@ -0,0 +1,37 @@
+//
+//  JCToolKit.h
+//  Pods
+//
+//  Created by Jam on 16/12/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#if __has_include(<JCToolKit/JCToolKit.h>)
+
+//! Project version number for JCToolKit.
+FOUNDATION_EXPORT double JCToolKitVersionNumber;
+
+//! Project version string for JCToolKit.
+FOUNDATION_EXPORT const unsigned char JCToolKitVersionString[];
+
+#import <JCToolKit/JCDefine.h>
+#import <JCToolKit/JCToolKit_Core.h>
+#import <JCToolKit/JCToolKit_Network.h>
+#import <JCToolKit/JCToolKit_UI.h>
+#import <JCToolKit/JCRouter.h>
+#import <JCToolKit/JCMediatorProtocol.h>
+#import <JCToolKit/Masonry.h>
+
+#else
+
+#import "JCDefine.h"
+#import "JCToolKit_Core.h"
+#import "JCToolKit_Network.h"
+#import "JCToolKit_UI.h"
+#import "JCRouter.h"
+#import "JCMediatorProtocol.h"
+#import "Masonry.h"
+
+#endif /* JCToolKit_h */
